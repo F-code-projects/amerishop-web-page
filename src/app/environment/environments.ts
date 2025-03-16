7af2a325eded67fc24ce955e0a262725b0d213cf
FrancoCarrillo <u202013943@upc.edu.pe>
2025-03-16 15:19:37 -0500

    fea: add google analytics

--- src/app/environment/environments.ts
+++ src/app/environment/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    production: false,
-    API_URL: 'https://kphduronynuhdislddwg.supabase.co/rest/v1',
-    googleAnalyticsId: 'G-C1H9L13TD7'
+  production: false,
+  API_URL: 'https://kphduronynuhdislddwg.supabase.co/rest/v1',
+  googleAnalyticsId: 'G-C1H9L13TD7',
 };
